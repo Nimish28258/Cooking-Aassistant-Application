35de8a940fc7ffacda8f6d5b49169bf00761dd57
Nimish28258 <nimishmalekar28258@gmail.com>
2025-02-03 03:41:01 +0530

    Update

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = "https://cooking-assistant-backend.onrender.com";
 
 export const registerUser = async (name, email, password) => {
   try {
